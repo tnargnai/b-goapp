f13e33e4ba9883da28d232cb9b914a2bd8ea3169
ian.grant <tnargnai@gmail.com>
2016-10-06 14:05:31 +0100

    bumped version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ian.grant@barclays.com'
 license 'all_rights'
 description 'Installs/Configures b-goapp'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.3'
+version '0.1.4'
 
 depends 'apt'
 depends 'yum'
